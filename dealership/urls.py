49255ac2681d946315b654b4cca4a950596b10bc
SaidAbbosxon <mr.khan.r35@gmail.com>
2024-10-13 20:47:12 +0500

    auth, dashboard changes

--- dealership/urls.py
+++ dealership/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
-from .views import index
+from .views import index, info, login_view, logout_view, register_view, dashboard
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('info/', info, name = 'info'),
+    
+    path('login/', login_view, name = 'login'),
+    path('logout/', logout_view, name = 'logout'),
+    path('register/', register_view, name = 'register'),
+    
+    path('dashboard/', dashboard, name = 'dash'),
 ]
--- dealership/views.py
+++ dealership/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-from .models import WellcomeHerro, Vehicles, Services, ClientsReviews, Partner
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
-from django.shortcuts import render
+from .forms import UserRegistrationForm
 from .models import WellcomeHerro, Vehicles, Services, ClientsReviews, Partner, TopBrands
 
 def index(request):
+    return render(request, 'index.html')
+
+
+def info(request):
     # Fetch all hero objects
     heroes = WellcomeHerro.objects.all()
 
@@ -35,12 +40,70 @@ def index(request):
         'conditions': conditions,
         'latest_vehicles': latest_vehicles,
         'services': services,
-        'client_reviews': client_reviews,
+        'clients_reviews': client_reviews,
         'partners': partners,
         'top_brands': top_brands,
     }
 
-    return render(request, 'index.html', context)
+    return render(request, 'info.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('info')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid form submission.')
+    else:
+        form = AuthenticationForm()
+        
+    return render(request, 'login.html', {f'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('info')
+    
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Registration successful.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Invalid form submission.')
+    else:
+        form = UserRegistrationForm()
+        
+    return render(request, 'register.html', {'form': form})
+
+def dashboard(request):
+
+    welcome = WellcomeHerro.objects.all()
+    vehicles = Vehicles.objects.all()
+    services = Services.objects.all()
+    client_reviews = ClientsReviews.objects.all()
+    partners = Partner.objects.all()
+    top_brands = TopBrands.objects.all()
+    
+    context = {
+        'wellcome_herro':welcome,
+        'vehicles':vehicles,
+        'services':services,
+        'client_reviews':client_reviews,
+        'partners':partners,
+        'top_brands':top_brands,
+    }
 
 
+    return render(request, 'dashboard.html', context)
 
